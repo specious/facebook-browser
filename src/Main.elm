e3eb72212b6d5f6b42d390f8560c16571ab647b9
Ildar Sagdejev <specious@gmail.com>
2017-01-29 01:10:28 -0500

    Search filter works

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Html.Attributes exposing (placeholder, href)
 import Html.Events exposing (onInput)
 import Json.Decode exposing (string, list, Decoder)
 import Json.Decode.Pipeline exposing (decode, required)
+import Regex
 import Http
 
 
@@ -30,7 +31,7 @@ type alias Items = List Item
 
 
 type alias Model =
-  { currentSearch : String
+  { searchStr : String
   , maybeItems : Maybe Items
   }
 
@@ -74,7 +75,7 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Change newContent ->
-      ({ model | currentSearch = newContent }, Cmd.none)
+      ({ model | searchStr = newContent }, Cmd.none)
 
     DataLoaded (Ok newItems) ->
       ({ model | maybeItems = Just newItems }, Cmd.none)
@@ -97,16 +98,22 @@ view : Model -> Html Msg
 view model =
   div []
     [ input [ placeholder "Search...", onInput Change ] []
-    , viewItems model.maybeItems
+    , viewItems model.searchStr model.maybeItems
     ]
 
-viewItems : Maybe Items -> Html Msg
-viewItems maybeItems =
+viewItems : String -> Maybe Items -> Html Msg
+viewItems searchStr maybeItems =
   case maybeItems of
     Nothing ->
       div [] [ text "Loading..." ]
     Just items ->
-      ul [] (List.map viewItem items)
+      let
+        filteredItems = List.filter (itemFilter searchStr) items
+      in
+        ul [] (List.map viewItem filteredItems)
+
+itemFilter searchStr item =
+  Regex.contains ((Regex.regex >> Regex.caseInsensitive) searchStr) item.title
 
 viewItem : Item -> Html Msg
 viewItem item =
