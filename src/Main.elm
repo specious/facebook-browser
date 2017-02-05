065b528fec90a71e28e27c5b8df7406bfe7c4ee8
Ildar Sagdejev <specious@gmail.com>
2017-02-05 17:18:14 -0500

    Switch between alternate view modes: title, thumbnail, thumnail with title

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (placeholder, id, href)
-import Html.Events exposing (onInput)
+import Html exposing (Html, text, div, img, a, span, h3, ul, li, input, button)
+import Html.Attributes exposing (placeholder, id, style, href, src)
+import Html.Events exposing (onInput, onClick)
 import Json.Decode exposing (string, list, Decoder)
 import Json.Decode.Pipeline exposing (decode, required)
 import Regex
@@ -33,6 +33,12 @@ main =
 -- # Model
 
 
+type ViewMode
+  = Text
+  | Thumbnails
+  | ThumbnailsWithTitle
+
+
 type alias Items =
   List Item
 
@@ -40,12 +46,13 @@ type alias Items =
 type alias Model =
   { searchStr : String
   , maybeItems : Maybe Items
+  , viewMode : ViewMode
   }
 
 
 init : ( Model, Cmd Msg )
 init =
-  ( Model "" Nothing
+  ( Model "" Nothing Text
   , getItemData
   )
 
@@ -75,6 +82,9 @@ itemsDecoder =
 type Msg
   = NoOp
   | Change String
+  | SetViewText
+  | SetViewThumbnails
+  | SetViewThumbnailsWithTitle
   | DataLoaded (Result Http.Error Items)
 
 
@@ -91,6 +101,15 @@ update msg model =
 
     Change newContent ->
       ( { model | searchStr = newContent }, Cmd.none )
+
+    SetViewText ->
+      ( { model | viewMode = Text }, Cmd.none )
+
+    SetViewThumbnails ->
+      ( { model | viewMode = Thumbnails }, Cmd.none )
+
+    SetViewThumbnailsWithTitle ->
+      ( { model | viewMode = ThumbnailsWithTitle }, Cmd.none )
 
     DataLoaded (Ok newItems) ->
       ( { model | maybeItems = Just newItems }, Task.attempt (\_ -> NoOp) (Dom.focus "search") )
@@ -116,12 +135,15 @@ view : Model -> Html Msg
 view model =
   div []
     [ input [ id "search", placeholder "Search...", onInput Change ] []
-    , viewItems model.searchStr model.maybeItems
+    , button [ onClick SetViewText ] [ text "Title" ]
+    , button [ onClick SetViewThumbnails ] [ text "Thumb" ]
+    , button [ onClick SetViewThumbnailsWithTitle ] [ text "Thumb + Title" ]
+    , viewItems model.searchStr model.maybeItems model.viewMode
     ]
 
 
-viewItems : String -> Maybe Items -> Html Msg
-viewItems searchStr maybeItems =
+viewItems : String -> Maybe Items -> ViewMode -> Html Msg
+viewItems searchStr maybeItems viewMode =
   case maybeItems of
     Nothing ->
       div [] [ text "Loading..." ]
@@ -131,14 +153,59 @@ viewItems searchStr maybeItems =
         filteredItems =
           List.filter (itemFilter searchStr) items
       in
-        ul [] (List.map viewItem filteredItems)
+        ul [] (List.map (viewItem viewMode) filteredItems)
 
 
 itemFilter searchStr item =
   Regex.contains ((Regex.regex >> Regex.caseInsensitive) searchStr) item.title
 
 
-viewItem : Item -> Html Msg
-viewItem item =
-  li []
-    [ a [ href ("https://www.facebook.com/" ++ item.id) ] [ text item.title ] ]
+viewItem : ViewMode -> Item -> Html Msg
+viewItem viewMode item =
+  case viewMode of
+    Text ->
+      li
+        []
+        [ a [ href ("https://www.facebook.com/" ++ item.id) ] [ text item.title ] ]
+
+    _ ->
+      viewItemWithThumbnail viewMode item
+
+
+viewThumbnail id styles =
+  a
+    [ href ("https://www.facebook.com/" ++ id)
+    , style styles
+    ]
+    [ img [ src ("./data/images/" ++ id) ] [] ]
+
+
+viewItemWithThumbnail viewMode item =
+  let
+    listStyle =
+      [ ( "list-style-type", "none" )
+      , ( "display", "inline-block" )
+      ]
+  in
+    case viewMode of
+      Thumbnails ->
+        li
+          [ style listStyle ]
+          [ viewThumbnail item.id [] ]
+
+      ThumbnailsWithTitle ->
+        li
+          [ style <| List.append listStyle [ ( "display", "flex" ) ] ]
+          [ viewThumbnail item.id [ ( "margin-right", "8px" ) ]
+          , h3
+              [ style
+                  [ ( "margin", "auto" )
+                  , ( "margin-left", "0" )
+                  , ( "color", "blue" )
+                  ]
+              ]
+              [ text item.title ]
+          ]
+
+      _ ->
+        div [] [ text "Should never happen" ]
